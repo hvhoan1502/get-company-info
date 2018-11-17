86c2ba85918ed775af9404ac5493d9168de3cc2b
Hoan Hoang <hvhoan1502@gmail.com>
2018-11-17 14:24:48 +0700

    Fix UI, add video, lib, company view

--- src/controllers/company.router.js
+++ src/controllers/company.router.js
@@ -10,17 +10,27 @@ companyRouter.use(mustBeUser);
 // Check body request when method is POST
 companyRouter.use((req, res, next) => {
     const body = req.body;
+    let allow = true;
     if (req.method === 'POST') {
         if (body.city.length === 0 || body.startPage.length === 0 || body.endPage.length === 0) {
-            return res.send('Not Found!');
+            allow = false;
         }
         try {
             req.body.startPage = parseInt(body.startPage);
             req.body.endPage = parseInt(body.endPage);
+            if (req.body.startPage > req.body.endPage) {
+                allow = false;
+            }
             
         } catch (err) {
-            return res.send('Invalid value!!!');
+            allow = false;
         }
+    }
+    if (!allow) {
+        const data = {
+            message: "Failled Action. Check enter information."
+        }
+        return res.render('pages/private/thongtincongty', { data, header: {} });
     }
     next();
 });
@@ -31,12 +41,17 @@ companyRouter.get('/', (req, res ) => {
 
 // Get thongtincongty.com information
 companyRouter.get('/thongtincongty', (req, res) => {
-    res.render('pages/private/thongtincongty', { data: null });
+    res.render('pages/private/thongtincongty', { data: null, header: {} });
 });
 
 companyRouter.post('/thongtincongty', (req, res) => {
     const body = req.body;
-    
+    const header = Object.assign({},
+        ({ city: body.city } || {}),
+        ({ district: body.district } || {}),
+        ({ startPage: body.startPage } || {}),   
+        ({ endPage: body.endPage } || {}) 
+    );
     const data = [
         {
             name: 'ABC1',
@@ -46,7 +61,7 @@ companyRouter.post('/thongtincongty', (req, res) => {
             field: 'Hi4'
         }
     ];
-    res.render('pages/private/thongtincongty', { data });
+    res.render('pages/private/thongtincongty', { header, data });
 });
 
 module.exports = { companyRouter }
