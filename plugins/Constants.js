0a87f98388b49b1cfef1b0c5b743d47be235fa73
Hoan Hoang <hvhoan1502@gmail.com>
2018-11-18 19:18:16 +0700

    Done site thongtincongty.

--- plugins/Constants.js
+++ plugins/Constants.js
@@ -4,6 +4,11 @@ const Login = {
     passwordHash: '$2b$08$gZUSruXorI761813y7VCO.g96t.YmcB2uw/nE6bKH2.aUngbAKte6'
 }
 
+const UrlSite = {
+    ThongTinCongTy: 'http://www.thongtincongty.com/'
+}
+
 module.exports = {
-    Login
+    Login,
+    UrlSite
 };
